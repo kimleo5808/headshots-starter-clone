c37e0e03cacb28005d802071b185dcbfe34f5794
kimleo5808 <kimleo5808@gmail.com>
2025-06-20 11:28:23 +0800

    Update ThemeProvider.tsx

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -2,8 +2,16 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes"
+import { type ThemeProviderProps } from "next-themes/dist/types"
+import Navbar from "@/components/Navbar"
+import { Toaster } from "@/components/ui/toaster"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider {...props}>
+      <Navbar />
+      <main className="pt-28 sm:pt-36">{children}</main>
+      <Toaster />
+    </NextThemesProvider>
+  )
 }
