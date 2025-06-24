f7d53d1262dd5366c8e9a1a576abd2c4c41c55c4
kimleo5808 <kimleo5808@gmail.com>
2025-06-24 13:20:19 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-// import { Toaster } from "@/components/ui/toaster"; // 删除了这一行
+// import Navbar from "@/components/Navbar"; // 删除了这一行
 
 export const metadata = {
   title: "Headshots AI",
@@ -17,9 +16,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={GeistSans.className}>
         <div className="relative flex min-h-screen flex-col">
-          <Navbar />
+          {/* <Navbar /> */} {/* 删除了这一行 */}
           <main className="flex-1">{children}</main>
-          {/* <Toaster /> */} {/* 删除了这一行 */}
         </div>
       </body>
     </html>
