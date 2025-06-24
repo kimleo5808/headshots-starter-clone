467eea695a4583164c019dca5a655b636016fb7d
kimleo5808 <kimleo5808@gmail.com>
2025-06-24 11:14:25 +0800

    Update ThemeProvider.tsx

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -2,8 +2,13 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-// 这是正确的 import 方式，直接从 "next-themes" 导入
-import { type ThemeProviderProps } from "next-themes";
+// 在 0.3.0 版本中，我们不需要从这里导入类型
+
+// 我们直接使用 React 的类型，这样更通用、更稳定
+type ThemeProviderProps = {
+  children: React.ReactNode;
+  [key: string]: any;
+};
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
