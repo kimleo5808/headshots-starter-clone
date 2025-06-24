5b31d83440bd59c06e7e920429acd840e841db2d
kimleo5808 <kimleo5808@gmail.com>
2025-06-24 10:42:58 +0800

    Update ThemeProvider.tsx

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -1,8 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes"
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes"
 import Navbar from "@/components/Navbar"
 import { Toaster } from "@/components/ui/toaster"
 
